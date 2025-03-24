3b0e7aa2055fb868d2a29be02d2c90b3ab96003b
ShamansGit <56842156+ShamansGit@users.noreply.github.com>
2025-03-24 17:06:52 +0000

    Fixed Commit

--- Asteroids/Asteroid.cs
+++ Asteroids/Asteroid.cs
@@ -105,7 +105,7 @@ public partial class Asteroid : Area2D
             for (int i = 0; i < chunkCount; i++)
             {
                 float radius = (collisionShape.Shape as CircleShape2D).Radius;
-                float spreadAngle = (float)i / (chunkCount - 1) * Mathf.Pi;
+                float spreadAngle = (float)i / chunkCount * Mathf.Pi * 2;
                 Vector2 spreadDirection = new Vector2(Mathf.Sin(spreadAngle),Mathf.Cos(spreadAngle));
                 Vector2 spreadOffset = spreadDirection * radius / 2f;
                 if (hitAsteroid is null)
